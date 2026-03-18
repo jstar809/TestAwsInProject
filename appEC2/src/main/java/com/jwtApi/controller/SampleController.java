66fb3dc48e911a99b77504e47b079759d81cf549
LEEJUNSEON <jstar809@contentwise.co.kr>
2026-03-18 14:24:17 +0900

    init

---
+++ appEC2/src/main/java/com/jwtApi/controller/SampleController.java
@@ -0,0 +1,18 @@
+package com.jwtApi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/api/sample")
+public class SampleController {
+	
+	@GetMapping("/getArr")
+	public String[] getArr() {
+		return  new String[] {"AAA" ,"BBB" , "CCC"};
+	}
+	
+}
---
+++ appEC2/src/test/java/com/jwtApi/AppEc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwtApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppEc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
